ece5e267f728c98add231ac8da272ae6a7e51fc6
romulo max <maxromulo16@gmail.com>
2022-12-15 19:39:29 -0300

    :recycle:refactor: alogando a pasta api para a pasta script

--- 7dayChallenge-Movie/script/script.js
+++ 7dayChallenge-Movie/script/script.js
@@ -1,19 +1,33 @@
 
-import { api_key } from "../api/apiKey.js";
+import { api_key } from "./api/apiKey.js";
 
 window.onload = async function(){
 
     const movie  = await getPupularFilmes();
+    movie.forEach(movie => renderMovie(movie))
 
 }
-
-
 
 async function getPupularFilmes() {
     const url = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key.api}`;
     const fetchresponse = await fetch(url);
     const { results }= await fetchresponse.json();
     return results
+
+}
+
+function renderMovie (movie) {
+    const {id, overview, poster_path, title, release_date, vote_average } = movie;
+
+    const ano = release_date.split('-')[0];
+    const img = `https://image.tmdb.org/t/p/w500${poster_path}`
+
+    const movieElement = document.createElement('div');
+    movieElement.classList.add('movie')
+    movieElement.setAttribute('id', id);
+
+    console.log(movieElement)
+
 
 }
 

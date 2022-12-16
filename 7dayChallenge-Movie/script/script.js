835621bb9dafdd0f5457877ab5a47d0d982d7f5e
romulo max <maxromulo16@gmail.com>
2022-12-15 22:42:26 -0300

    :construction:feat: Add function renderMovie

--- 7dayChallenge-Movie/script/script.js
+++ 7dayChallenge-Movie/script/script.js
@@ -1,6 +1,8 @@
 
 import { api_key } from "./api/apiKey.js";
 
+
+const movieContainer = document.querySelector('.container')
 window.onload = async function(){
 
     const movie  = await getPupularFilmes();
@@ -23,19 +25,68 @@ function renderMovie (movie) {
     const img = `https://image.tmdb.org/t/p/w500${poster_path}`
 
     const movieElement = document.createElement('div');
+
     movieElement.classList.add('movie')
     movieElement.setAttribute('id', id);
+    movieContainer.appendChild(movieElement)
 
 
     //POST FILVE
 
     const cardMovie = document.createElement('figure');
-
     const imgMovie = document.createElement('img');
     imgMovie.src = img;
     imgMovie.classList.add('img');
-    imgMovie.appendChild(cardMovie);
-    cardMovie.appendChild(movieElement)
+    cardMovie.appendChild(imgMovie);
+    movieElement.appendChild(cardMovie)
+
+    //acctiosn 
+
+    const cardInfo = document.createElement('div');
+    cardInfo.classList.add('actions');
+    movieElement.appendChild(cardInfo);
+    
+    // title
+    const titleMovie = document.createElement('h4');
+    titleMovie.innerHTML = title;
+    titleMovie.classList.add('titulo-filme');
+    cardInfo.appendChild(titleMovie);
+
+    // infos 
+    const infos = document.createElement('div')
+    infos.classList.add('informacoes');
+    cardInfo.appendChild(infos);
+    
+    
+    // start
+    const cardStar = document.createElement('figure')
+    cardStar.classList.add('star')
+    const imgStar = document.createElement('img');
+    imgStar.classList.add('star');
+    imgStar.src = './img/star.png';
+    const span = document.createElement('span');
+    span.innerHTML = vote_average;
+    
+    cardStar.appendChild(imgStar)
+    cardStar.appendChild(span);
+    infos.appendChild(cardStar);
+
+    
+    //coração/
+    const cardCora = document.createElement('figure')
+    const imgCora = document.createElement('img');
+    imgCora.classList.add('cora')
+    imgCora.src = './img/heart.png';
+    cardCora.appendChild(imgCora);
+    infos.appendChild(cardCora);
+
+    //para
+
+    const cardarticle = document.createElement('article');
+    cardarticle.classList.add('descricao');
+    cardarticle.innerHTML = overview;
+    movieElement.appendChild(cardarticle)
+
 
 
 }

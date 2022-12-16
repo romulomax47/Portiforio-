88f32cd719f39068c2d52c522c732b40ed7e47c1
romulo max <maxromulo16@gmail.com>
2022-12-16 18:28:55 -0300

    :construction:feat: Add function serchMovie

--- 7dayChallenge-Movie/script/script.js
+++ 7dayChallenge-Movie/script/script.js
@@ -1,27 +1,41 @@
 
 import { api_key } from "./api/apiKey.js";
+import  getMovie from './getMovies.js';
+
+// btn pesquisar movie
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', serchMovie);
 
 
 const movieContainer = document.querySelector('.container')
 window.onload = async function(){
 
-    const movie  = await getPupularFilmes();
+    const movie  = await getMovie();
     movie.forEach(movie => renderMovie(movie))
 
 }
 
-async function getPupularFilmes() {
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key.api}`;
-    const fetchresponse = await fetch(url);
-    const { results }= await fetchresponse.json();
-    return results
 
+
+async function serchMovie() {
+    const formTitle = document.querySelector('#input-movie').value;
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key.api}&query= ${ formTitle } &language=en-US&page=1`
+
+    const get = await fetch(url);
+    
+    const {results} = await get.json();
+
+    movieContainer.innerHTML = '';
+    results.forEach(movie => renderMovie(movie));
 }
+
+
 
 function renderMovie (movie) {
     const {id, overview, poster_path, title, release_date, vote_average } = movie;
 
-    const ano = release_date.split('-')[0];
     const img = `https://image.tmdb.org/t/p/w500${poster_path}`
 
     const movieElement = document.createElement('div');
@@ -31,8 +45,6 @@ function renderMovie (movie) {
     movieContainer.appendChild(movieElement)
 
 
-    //POST FILVE
-
     const cardMovie = document.createElement('figure');
     const imgMovie = document.createElement('img');
     imgMovie.src = img;
@@ -40,25 +52,21 @@ function renderMovie (movie) {
     cardMovie.appendChild(imgMovie);
     movieElement.appendChild(cardMovie)
 
-    //acctiosn 
 
     const cardInfo = document.createElement('div');
     cardInfo.classList.add('actions');
     movieElement.appendChild(cardInfo);
     
-    // title
     const titleMovie = document.createElement('h4');
     titleMovie.innerHTML = title;
     titleMovie.classList.add('titulo-filme');
     cardInfo.appendChild(titleMovie);
 
-    // infos 
     const infos = document.createElement('div')
     infos.classList.add('informacoes');
     cardInfo.appendChild(infos);
     
     
-    // start
     const cardStar = document.createElement('figure')
     cardStar.classList.add('star')
     const imgStar = document.createElement('img');
@@ -86,8 +94,6 @@ function renderMovie (movie) {
     cardarticle.classList.add('descricao');
     cardarticle.innerHTML = overview;
     movieElement.appendChild(cardarticle)
-
-
 
 }
 

d520d4e190b7800202b3ebd3824a61fe6597c149
romulo max <maxromulo16@gmail.com>
2022-12-20 19:22:19 -0300

    :construction:feat: Add function getMovies

--- 7dayChallenge-Movie/script/getMovies.js
+++ 7dayChallenge-Movie/script/getMovies.js
@@ -1,7 +1,7 @@
 import {api_key } from './api/apiKey.js'
 
 
-async function getMovie () {
+async function getMovie () {                       // popular
     const url = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key.api}`;
     const fetchresponse = await fetch(url);
     const { results }= await fetchresponse.json();
